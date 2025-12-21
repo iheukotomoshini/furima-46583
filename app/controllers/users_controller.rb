a89ae6de31c7735b9c16e1017df1493361a91810
kyouseikuma <nishikou1031@gmail.com>
2025-12-21 15:23:06 +0900

    ユーザー管理機能first再

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,13 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to some_path
+    else
+      @unique_errors = @user.errors.full_messages.uniq
+      render :new
+    end
+  end
 end
