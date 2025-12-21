1d0e783f3027a465ba8001ed4f074976c0f325fe
kyouseikuma <nishikou1031@gmail.com>
2025-12-21 17:16:48 +0900

    ユーザー管理機能second_ログイン機能修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,11 +4,11 @@ class UsersController < ApplicationController
   end
   def create
     @user = User.new(user_params)
-    if @user.save
-      redirect_to some_path
-    else
-      @unique_errors = @user.errors.full_messages.uniq
-      render :new
-    end
+ #   if @user.save
+#      redirect_to some_path
+#    else
+#      @unique_errors = @user.errors.full_messages.uniq
+#      render :new
+#    end
   end
 end
