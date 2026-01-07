ebefc509409cf60f6b515baf6fa5f07e46dbe091
kyouseikuma <nishikou1031@gmail.com>
2026-01-07 23:11:24 +0900

    商品情報編集機能_1st

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -1,17 +1,15 @@
 class FurimasController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
-  
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_common_resources, only: [:new, :edit, :create, :update]
+  before_action :set_furima, only: [:edit, :update]
+  before_action :check_owner, only: [:edit, :update]
+
   def index
     @furimas = Furima.order(updated_at: :desc)
   end
 
   def new
     @furima = Furima.new
-    @genres = Genre.all
-    @statuses = Status.all
-    @fees = Fee.all
-    @prefectures = Prefecture.all
-    @scheduled_deliveries = ScheduledDelivery.all
   end
 
   def create
@@ -20,11 +18,6 @@ class FurimasController < ApplicationController
     if @furima.save
       redirect_to root_path
     else
-      @genres = Genre.all
-      @statuses = Status.all
-      @fees = Fee.all
-      @prefectures = Prefecture.all
-      @scheduled_deliveries = ScheduledDelivery.all
       render :new, status: :unprocessable_entity
     end
   end
@@ -33,12 +26,36 @@ class FurimasController < ApplicationController
     @furima = Furima.find(params[:id])
   end
 
+  def edit
+  end
+
+  def update
+    if @furima.update(furima_params)
+      redirect_to furima_path(@furima)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
+  def set_common_resources
+    @genres = Genre.all
+    @statuses = Status.all
+    @fees = Fee.all
+    @prefectures = Prefecture.all
+    @scheduled_deliveries = ScheduledDelivery.all
+  end
 
+  def set_furima
+    @furima = Furima.find(params[:id])
+  end
+
+  def check_owner
+    redirect_to root_path unless @furima.user == current_user
+  end
 
   def furima_params
     params.require(:furima).permit(:image, :title, :explanation, :category_id, :status_id, :shipping_fee_status_id, :prefecture_id, :scheduled_delivery_id, :price)
   end
-
 end
